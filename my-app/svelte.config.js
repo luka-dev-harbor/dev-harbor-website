f1bb07212751473d6ffd73728c4afe96f1b9f2ee
Luka Stanisic <luka@dev-harbor.com>
2025-04-17 16:52:46 +0200

    From scratch with svelteKit

--- my-app/svelte.config.js
+++ my-app/svelte.config.js
@@ -1,15 +1,19 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
-	}
+			fallback: 'index.html'
+		}),
+		paths: {
+			base: '/luka-dev-harbor.github.io'
+		}
+	},
+	preprocess: vitePreprocess()
 };
+
+export default config;
