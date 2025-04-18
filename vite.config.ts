022cb59720ce8da8d55e5f6f84a4fc39732a6093
Luka Stanisic <luka@dev-harbor.com>
2025-04-18 19:51:43 +0200

    Add url to viteconfig as well

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+const dev = process.env.NODE_ENV === 'development';
+
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	base: dev ? '' : '/dev-harbor-website/'
 });
