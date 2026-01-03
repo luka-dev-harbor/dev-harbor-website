d7da9261bd8bf67736f325b1dd9b0a4be9c7bb2a
Luka Stanisic <luka@dev-harbor.com>
2026-01-03 19:22:49 +0100

    Improve header color transitions

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -198,7 +198,7 @@
         .navbar {
             .navbar-collapse {
                 background-color: #fff;
-                box-shadow: rgba(0, 0, 0, 0.1) 0px 20px 25px -5px, rgba(0, 0, 0, 0.04) 0px 10px 10px -5px;
+                box-shadow: rgba(0, 0, 0, 0.1) 0 20px 25px -5px, rgba(0, 0, 0, 0.04) 0 10px 10px -5px;
                 padding: 1rem;
                 margin-top: 1rem;
                 border-radius: 8px;
