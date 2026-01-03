45464d42b66ff645cb07b4c14f146e4ab71943fc
Luka Stanisic <luka@dev-harbor.com>
2026-01-03 17:30:38 +0100

    Fix base path

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,10 +8,7 @@ const config = {
 			pages: 'build',
 			assets: 'build',
 			fallback: 'index.html'
-		}),
-		paths: {
-			base: dev ? '' : '/dev-harbor-website'
-		},
+		})
 	},
 	preprocess: vitePreprocess()
 };
