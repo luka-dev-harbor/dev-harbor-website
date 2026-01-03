e4bb1b9a908d5a6d3a1aff360cc21ec95b762cf7
Luka Stanisic <luka@dev-harbor.com>
2026-01-03 17:48:35 +0100

    fix vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-const dev = process.env.NODE_ENV === 'development';
-
 export default defineConfig({
-	plugins: [sveltekit()],
-	base: dev ? '' : '/dev-harbor-website/'
+	plugins: [sveltekit()]
 });
